6dd9b87110900d8fffd9ce69a1dc37176b2fc993
texmorgan <texmorgan@gmail.com>
2014-04-06 19:59:15 -0500

    Removed reference to old database configurations.

--- tests/unit/core/basicAgencyTest.php
+++ tests/unit/core/basicAgencyTest.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once __DIR__ . DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."config".DIRECTORY_SEPARATOR."config.inc.php";
-//require_once AMPHIBIAN_DATABASE."Coworks.In.mysql.config.inc.php";
 require_once AMPHIBIAN_TESTS . "baseTest.php";
 require_once AMPHIBIAN_CORE_ABSTRACT . "BasicAgency.php";
 /**
--- tests/unit/core/basicInteractionTest.php
+++ tests/unit/core/basicInteractionTest.php
@@ -1,6 +1,5 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."config".DIRECTORY_SEPARATOR."config.inc.php";
-require_once AMPHIBIAN_DATABASE."Coworks.In.mysql.config.inc.php";
 require_once AMPHIBIAN_TESTS."baseTest.php";
 require_once AMPHIBIAN_CORE_ABSTRACT . "BasicInteraction.php";
 /**
